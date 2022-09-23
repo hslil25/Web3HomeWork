2fb077e99581d6bc548bef9b95d8dc98084eb0c2
Remix Workspace <>
2022-09-23 22:55:52 +0300

    Upload

---
+++ Homework.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract ETHStore {
+uint256 public balance;
+address public owner;
+
+constructor() {
+    owner = msg.sender;
+}
+
+receive() payable external {
+    balance += msg.value;
+}
+
+modifier onlyowner(){
+    //Trying to keep error messages short to save gas.
+    require(msg.sender==owner, "Unauthorized");
+    _;
+}
+
+function withdraw(uint amount, address payable destination) public onlyowner{
+    require(amount <= balance, "Insufficient");
+    destination.transfer(amount);
+    balance = balance - amount;
+}
+}
