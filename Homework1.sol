c0bf11d0251c0ba55a31e5dfb959a2f186cdfdcb
Halil <91384981+hslil25@users.noreply.github.com>
2022-09-26 18:34:40 +0300

    Update Homework1.sol

--- Homework1.sol
+++ Homework1.sol
@@ -9,18 +9,22 @@ constructor() {
     owner = msg.sender;
 }
 
+//Adds to balance
 receive() payable external {
     balance += msg.value;
 }
 
+//To code in a more clean way I am creating a modifier.
 modifier onlyowner(){
     //Trying to keep error messages short to save gas.
     require(msg.sender==owner, "Unauthorized");
     _;
 }
 
+//Only owner can withdraw money so modified with onlyowner() modifier
 function withdraw(uint amount, address payable destination) public onlyowner{
     require(amount <= balance, "Insufficient");
+    //Placing this code before the second one in order to prevent hacking. 
     balance = balance - amount;
     destination.transfer(amount);
 }
